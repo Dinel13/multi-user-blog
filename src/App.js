a07b58cd2818e43d9d3fd914d886256ad2cbcdc4
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-04-25 13:15:21 +0800

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-black">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-2xl text-white">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
