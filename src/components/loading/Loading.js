b1ca4e3c70fcd3b86c7c09906f2a9986cd0b3805
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-04 11:16:19 +0800

    add loading when fetch penulis pop

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import style from "./loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className="w-full z-50 flex flex-col items-center justify-center mb-5">
+      <div
+        className={`${style.loader} ease-linear rounded-full border-8 border-t-8 border-gray-400 h-12 w-12 mb-4`}
+      ></div>
+      <h2 className="text-center text-gray-700 text-xl font-semibold">
+        Loading...
+      </h2>
+      <p className="w-1/3 text-center text-gray-600">
+        Kami butuh sedikit waktu untuk mendapatkan data dari server.
+      </p>
+    </div>
+  );
+}
