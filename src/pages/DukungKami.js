44d0ee512d40c9806d469f6e9668caa359115bd8
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-14 12:39:35 +0800

    beutifull dukung kami

--- src/pages/DukungKami.js
+++ src/pages/DukungKami.js
@@ -1,34 +1,153 @@
 import React from "react";
 
-import Unhas from "../assets/unhas.jpg";
-
 export default function DukungKami() {
   return (
-    <section class="text-gray-600 body-font">
-      <h1 class=" sm:text-4xl text-center mt-8 text-3xl font-medium block text-gray-900">
-        Ayo dukung Suara Unhas
-      </h1>
-      <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img
-            class="object-cover object-center rounded"
-            alt="hero"
-            src={Unhas}
-          />
-        </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 class="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">
-            Wadah Mahasiswa, <br class="hidden lg:inline-block" />
-            Mahasiswa Universitas Hasanuddin
-          </h1>
-          <p class="mb-8 leading-relaxed">
-            Suara unhas adalah sebuah media sosial. Dikembangkan untuk mehadahi
-            Mahasiswa khususnya mahasiswa unhas dibidang karya tulis. Media ini
-            kami harapkan menjadi wadah mahasiswa untuk merepresentasikan
-            dirinya. yaitu sebagai mahasiswa yang penuh akan ide dan gagasan.
-            mahasiswa yang menghasilkan karya yang mengispiorasi bagi sesamanya.
-            sehiangga ia dapat mengabadikan karyanya dalam ingatan orang lain
+    <section class="text-gray-600 body-font bg-gray-100">
+      <div class="container px-5 py-16 mx-auto flex flex-wrap">
+        <div className="mx-auto md:w-2/3 lg:w-8/12 mb-4">
+          <h2 className="text-center text-3xl text-gray-900 mb-4">
+            Dukung pengembang SuaraUnhas
+          </h2>
+          <p className="text-left">
+            Kamu bisa mendukung SuaraUnhas dengan banyak cara. Sesuai dengan
+            kemampuan yang kamu miliki. Beberapa caranya adalah seperti berikut
           </p>
+        </div>
+        <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-red-500 text-white relative z-10 title-font font-medium text-sm">
+            1
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 bg-red-100 text-red-500 rounded-full inline-flex items-center justify-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-12 h-12"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+              </svg>
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                Menulisa Kode
+              </h2>
+              <p class="leading-relaxed">
+                Bantu SuaraUnhas dengan bersama-sama menulis dan memperbaiki
+                kode kami dari halaman. Saat ini SauraUnhas mengunakan pustaka
+                ReactJS, Express, NodeJS, MonggoDB, Redux , Tailwind dan
+                beberapa lainya.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-red-500 text-white relative z-10 title-font font-medium text-sm">
+            2
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 bg-red-100 text-red-500 rounded-full inline-flex items-center justify-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-12 h-12"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                Sumbangan dana
+              </h2>
+              <p class="leading-relaxed">
+                SuaraUnhas membutuhkan dana untuk biaya sewa serta pemeliharaan
+                server serta domain. Sumbngana dana sekecil apapun dari kamu
+                akan sangat membantu kami untuk terus berkembang.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-red-500 text-white relative z-10 title-font font-medium text-sm">
+            3
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 bg-red-100 text-red-500 rounded-full inline-flex items-center justify-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-12 h-12"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="12" cy="5" r="3"></circle>
+                <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+              </svg>
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                Saran dan Kritik
+              </h2>
+              <p class="leading-relaxed">
+                Saran atau kritik yang membangun juga adalah bentuk dukungan
+                yang sangat berarti bagi kami. Karena sebagai penguna suaraUnhas
+                maka kamulah yang paling tahu apa yang masih harus kami perbaiki
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-red-500 text-white relative z-10 title-font font-medium text-sm">
+            4
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 bg-red-100 text-red-500 rounded-full inline-flex items-center justify-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-12 h-12"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                Menulis dan Bagikan
+              </h2>
+              <p class="leading-relaxed">
+                Terakhir, mari mulai menulis dan bagikan karya terbaik kamu di
+                suaraUnhas. karena semakin banyak kamu menulis dan membagikan
+                karyamu berati juga semakin banyak orang yang dapat memanfaatkan
+                SuaraUnhas
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/KrtikSaran.js
+++ src/pages/KrtikSaran.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function KririkSaran() {
+  const textRef = useRef("");
+  const emailRef = useRef("");
+  const submitHandler = () => {};
   return (
     <section className="text-gray-600 body-font relative">
       <div className="absolute inset-0 bg-gray-300">
@@ -16,22 +19,26 @@ export default function KririkSaran() {
           style={{ filter: "grayscale(1) contrast(1.5) opacity(0.6)" }}
         ></iframe>
       </div>
-      <div className="container px-5 py-24 mx-auto flex">
-        <d className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-2 shadow-md">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+      <div className="container px-5 py-16 mx-auto flex">
+        <form
+          onSubmit={submitHandler}
+          className="lg:w-5/12 md:w-4/5 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-2 shadow-md"
+        >
+          <h2 className="text-gray-900 text-xl mb-1 font-medium title-font">
             Kritik dan Saran
           </h2>
-          <p className="leading-relaxed mb-5 text-gray-600">
+          <p className="leading-none mb-5 text-gray-700">
             SuaraUnhas menerima kritik atu saran dari kamu untuk semakin
             memperbaiki layanan kami
           </p>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-700">
               Email
             </label>
             <input
               type="email"
-              id="email"
+              ref={emailRef}
+              required
               name="email"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
@@ -39,24 +46,25 @@ export default function KririkSaran() {
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-600"
+              className="leading-7 text-sm text-gray-700"
             >
-              Message
+              Pesan
             </label>
             <textarea
-              id="message"
+              ref={textRef}
+              required
               name="message"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             ></textarea>
           </div>
-          <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <button className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
             Kirim
           </button>
-          <p className="text-xs text-gray-500 mt-3">
+          <p className="text-xs leading-none text-gray-500 mt-3">
             kritik atau saran kamu dijaga kerahasianya dan hanya digunakan untuk
             keperluan SuaraUnhas
           </p>
-        </d>
+        </form>
       </div>
     </section>
   );
