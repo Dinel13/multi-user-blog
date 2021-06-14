2f588e12d91d41a6c11f554cae4a2f694a0d5d66
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-14 11:11:29 +0800

    Revert "beutiful tentang kami dan dev"
    This reverts commit 868b542d04a064cbb5f791a5f84ebd540caee690.

--- src/pages/TentangKami.js
+++ src/pages/TentangKami.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import Unhas from "../assets/unhas.jpg";
 
 export default function TentangKami() {
   return (
-    <section class="text-gray-600 body-font bg-gray-100">
-      <h1 class="pt-12 sm:text-4xl text-center text-3xl font-medium block text-gray-900">
+    <section class="text-gray-600 body-font">
+      <h1 class=" sm:text-4xl text-center mt-8 text-3xl font-medium block text-gray-900">
         Apa itu SuaraUnhas
       </h1>
-      <div class="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center md:items-start">
+      <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
           <img
             class="object-cover object-center rounded"
@@ -17,46 +16,18 @@ export default function TentangKami() {
             src={Unhas}
           />
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-16 md:pl-10 flex flex-col md:items-start md:text-left items-center text-center">
+        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 class="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">
             Wadah Mahasiswa, <br class="hidden lg:inline-block" />
-            <span className="text-gray-700">
-              Mahasiswa Universitas Hasanuddin
-            </span>
+            Mahasiswa Universitas Hasanuddin
           </h1>
-          <p class="mb-8 leading-relaxed text-gray-700">
-            SuaraUnhas adalah sebuah media sosial. Dikembangkan untuk Mahasiswa
-            unhas dalam hal literasi. Media ini kami harapkan menjadi wadah bagi
-            mahasiswa untuk menyurakan atau merepresentasikan dirinya. Karena
-            sebagai mahasiswa yang niscaya kaya akan ide dan gagasan, pasti
-            memerlukan media untuk penyaluran. Sebagaimana pelukis membutuhkan
-            kanvas maka SuaraUnhas ini dikembangkan untuk manjadi kanvasnya
-            mahasiswa kreatif.
-            <br />
-            <br />
-            Di SuaraUnhas kamu dapat menuliskan karyamu di halaman{" "}
-            <Link to="/tulis" className="text-indigo-600 font-bold">
-              Menulis{" "}
-            </Link>
-            . Kamu juga dapat membaca karya mahasiswa lain di halaman{" "}
-            <Link to="/baca" className="text-indigo-600 font-bold">
-              Menbaca{" "}
-            </Link>
-            . SuaraUnhas dikembangkan khusus bagi mahasiswa Universitas
-            Hasanuddin. Jadi, untuk dapat menulis disini kamu harus mendaftar
-            mengunakan email kampus yang memiliki domain{" "}
-            <span className="italic font-semibold">"@student.unhas.ac.id"</span>
-            . Email tersebut bisa didapatkan dari website{" "}
-            <a
-              className="text-indigo-600 font-bold"
-              href="https://sso.unhas.ac.id/index.php"
-              rel="noreferrer"
-              target="_blank"
-            >
-              SSO Unhas{" "}
-            </a>
-            . Namun jika kamu hanya ini memabaca maka kamu tidak harus memiliki
-            akun terlebih dahulu.
+          <p class="mb-8 leading-relaxed">
+            Suara unhas adalah sebuah media sosial. Dikembangkan untuk mehadahi
+            Mahasiswa khususnya mahasiswa unhas dibidang karya tulis. Media ini
+            kami harapkan menjadi wadah mahasiswa untuk merepresentasikan
+            dirinya. yaitu sebagai mahasiswa yang penuh akan ide dan gagasan.
+            mahasiswa yang menghasilkan karya yang mengispiorasi bagi sesamanya.
+            sehiangga ia dapat mengabadikan karyanya dalam ingatan orang lain
           </p>
         </div>
       </div>
